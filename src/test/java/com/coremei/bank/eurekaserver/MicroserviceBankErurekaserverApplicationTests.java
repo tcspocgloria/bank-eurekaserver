3caeea330bd74d722a7eb55f74b80396945caae1
TCS POC Gloria <tcspocgloria@gmail.com>
2021-08-31 15:09:30 -0500

    first commit

---
+++ src/test/java/com/coremei/bank/eurekaserver/MicroserviceBankErurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coremei.bank.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBankErurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
